32d8a4b3f3476520ae9cd58669d9140686fa6177
Gilson Cosme de Vasconcelos Junior <65792764+GilsonJrr@users.noreply.github.com>
2022-03-14 16:43:45 -0300

    Commit 02

--- src/App.js
+++ src/App.js
@@ -12,10 +12,13 @@ function App() {
 
   const[data, setData] = useState()
   const[ip, setIP] = useState('')
-  const[ipNumber, setIpNumber] = useState('')
+
   const[loading, setLoading] = useState(true)
-  const[position, setPosition] = useState([])
-  const[latLong, setLatLong] = useState([])
+  const[loadingMap, setLoadingMap] = useState(true)
+  const[empty, setEmpty] = useState(false)
+
+  let position = ([0,0])
+  let ipNumber = ('')
 
   function GetIcon() {
     return L.icon({
@@ -26,32 +29,59 @@ function App() {
 
   useEffect(() => {
 
-    async function CallData(){
-      await api
-      .get("v2/country,city?apiKey=at_CAVHz31La5XSme4sO4IBOWpbobQnB&ipAddress="+ipNumber)
-      .then((response) => setData(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-    }
-
-    // setLatLong([data?.location.lat, data?.location.lng])
-    setPosition([48.86696457626759, 2.33845037412616])
-    
-      if(position != ''){
-        setTimeout(() => {
-          setLoading(false)
-        }, 2000);
+      async function LoadAPI(){
+        const response = await api.get("json/"+ipNumber);
+        setData(response.data);
+        console.log(data)
+        setLoading(false);
       }
+      LoadAPI();
       
-  });
+  },[]);
 
-  function HandleIP(){
-    setIpNumber(ip)
-    setLoading(true)
-    setLatLong([40.42898475978098, -3.7070096998396393])
+  const refresh = () => {
+
+      setEmpty(false)
+      ipNumber = (ip)
+    
+      setLoadingMap(true)
+      setLoading(true)
+  
+      async function LoadAPI(){
+        const response = await api.get("json/"+ipNumber);
+        setData(response.data);
+        console.log(data)
+      }
+  
+      LoadAPI();
+  
+      position = ([data?.lat, data?.lon]);
+      setTimeout(()=>{
+        setLoading(false)
+      },500)
+    
+  };
+
+  useEffect(refresh, []);
+
+  if(data?.status === 'success'){
+    position = ([data?.lat, data?.lon]);
+    setTimeout(()=>{
+      setLoadingMap(false)
+    },500)
   }
 
+  function Error(){
+    if(data?.status === 'fail'){
+      setData('')
+      setEmpty(true)
+      setLoading(true)
+      setLoadingMap(true)
+    }
+  }
+
+  Error()
+  
   return (
     <div className='App'>
 
@@ -63,19 +93,19 @@ function App() {
             <input placeholder='Search for any IP or Domain' value={ip} 
             onChange={(e)=> setIP(e.target.value) }/>
           </div>
-          <div className='Arow' onClick={HandleIP}>
+          <div className='Arow' onClick={refresh}>
             <img src={Arrow}/>
           </div>
         </div>
       </div>
 
-      {loading ?
+      {loadingMap?
       <div className='Loading'>
         <Spinner animation="border" variant="dark" />
       </div>
       :
       <div className='Map'>
-        <MapContainer center={position} zoom={13} className='Map' >
+        <MapContainer center={position} zoom={13} className='Map' zoomControl={false}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -87,30 +117,33 @@ function App() {
           </Marker>
         </MapContainer>
       </div>}
-      
+
       { loading ?
         <div className='Info'>
           <div className='LoadingInfo'>
             <Spinner animation="border" variant="dark" />
           </div>
         </div>
-        :
+      :
       <div className='Info'>
         <div className='InfoTitles'>
           <p1>IP ADDRESS</p1>
-          <p2>{data?.ip}</p2>
+          <p2>{data?.query}</p2>
+          {/* <p2>{data?.ip}</p2> */}
           {/* <p2>8.8.8.8</p2> */}
         </div>
         <div className='Line'/>
         <div className='InfoTitles'>
           <p1>LOCATION</p1>
-          <p2>{data?.location.region}, {data?.location.country} {data?.as.asn}</p2>
+          {/* <p2>{data?.location.region}, {data?.location.country} {data?.as.asn}</p2> */}
+          <p2>{data?.region}, {data?.country} {data?.zip}</p2>
           {/* <p2>Brooklyn, NY 10001</p2> */}
         </div>
         <div className='Line'/>
         <div className='InfoTitles'>
           <p1>TIMEZONE</p1>
-          <p2>UTC{data?.location.timezone}</p2>
+          {/* <p2>UTC{data?.location.timezone}</p2> */}
+          <p2>{data?.timezone}</p2>
           {/* <p2>UTC - 05:00</p2> */}
         </div>
         <div className='Line'/>
@@ -120,6 +153,14 @@ function App() {
           {/* <p2>SpaceX Starlink</p2> */}
         </div>
       </div>}
+
+      {empty &&
+        <div className='Info'>
+          <div className='LoadingInfo'>
+            <p2>IP or Domain not found</p2>
+          </div>
+        </div>
+      }
 
     </div>
   );
--- src/App/API.js
+++ src/App/API.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://geo.ipify.org/api",
+  baseURL: "http://ip-api.com",
+  
 });
 
 export default api;
